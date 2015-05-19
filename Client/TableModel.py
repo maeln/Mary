6e836c1ca268ecfe15b868572c88bb8070bffdcd
Maël Naccache <contact@maeln.com>
2015-05-19 17:46:35 +0200

    Working on table model.

--- Client/TableModel.py
+++ Client/TableModel.py
@@ -9,3 +9,18 @@ class TableModel(QtCore.QAbstractItemModel):
 	def __init__(self, parent=None):
 		super(TableModel, self).__init__(parent)
 		self.row = ["File", "MIBD", "GOT INFO", "ON BDD"]
+		self.column = []
+
+	def rowCount(self):
+		return len(self.row)
+
+	def columnCount(self):
+		return len(self.column)
+
+	def data(self, index, role = QtCore.Qt.DisplayRole)
+		x = index.row()
+		y = index.column()
+		if self.rowCount() >= x:
+			if self.columnCount() >= y:
+				return self.column[y][x]
+		return QtCore.QVariant()
